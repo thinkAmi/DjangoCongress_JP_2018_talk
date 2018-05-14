5688c286950aaffa97e34d06b9f084a96e8d36fe
thinkAmi <dev.thinkami@gmail.com>
2018-05-15 07:20:57 +0900

    DeprecationMixinの使用例を修正

--- src/myproject/myproject/middlewares/deprecation_middleware.py
+++ src/myproject/myproject/middlewares/deprecation_middleware.py
@@ -2,7 +2,14 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class DeprecationMixinDjangoMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        print('[mixin] process_request')
+
+    def process_response(self, request, response):
+        print('[mixin] process_response')
+        return response
+
     def process_template_response(self, request, response):
-        print('[mixin] hook!')
+        print('[mixin] process_template_response')
         response.context_data['message'] = 'overwrite by deprecation mixin middleware'
         return response
